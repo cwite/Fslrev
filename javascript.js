9c909d2e7ff7573079e4bdf0cb958d2364ca1df1
Callum White <62078259+cwite@users.noreply.github.com>
2020-03-23 18:19:29 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function function1() {
+  document.getElementById("ff23s").innerHTML = "hello";
+}
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
